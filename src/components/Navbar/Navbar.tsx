dd6d49eda835a4dc1ba5db35573dc15e8eb9886e
Gonzalo Arca <goarca@itba.edu.ar>
2022-07-16 19:27:41 -0300

    :sparkles: add logo to navbar and navbar styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
+import styles from '../../assets/styles/Navbar.module.scss';
 
 const Navbar = () => {
 	const navigate = useNavigate();
@@ -9,15 +10,12 @@ const Navbar = () => {
 	};
 
 	return (
-		<div
-			style={{
-				padding: '0 5rem 0 2rem',
-				display: 'flex',
-				justifyContent: 'space-between',
-				alignItems: 'center',
-			}}
-		>
-			<span>OMNI LOGO</span>
+		<div className={styles.navbarContent}>
+			<img
+				className={styles.navbarLogo}
+				alt='omni logo'
+				src='img/omni-logo-white-2.svg'
+			/>
 			<Button onClick={navigateToLogin}>Go to the platform</Button>
 		</div>
 	);
