4fd3c998951059ee5a35c4c740877608e5b66cae
oserpe <oserpe@itba.edu.ar>
2022-07-16 22:06:55 -0300

    :recycle: refactor side menu and add messages screen

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -6,45 +6,65 @@ import {
 	LinkOutlined,
 } from '@ant-design/icons';
 import Menu, { MenuProps } from 'antd/lib/menu';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Role } from '../../models/authModels';
+import Routes from '../../routes/routes';
+import { getRole } from '../utils/session';
 import { Props } from '../utils/types';
 
 type MenuItem = Required<MenuProps>['items'][number];
 
-function getItem(
-	label: React.ReactNode,
-	key?: React.Key | null,
-	icon?: React.ReactNode,
-	children?: MenuItem[]
-): MenuItem {
-	return {
-		key,
-		icon,
-		children,
-		label,
-	} as MenuItem;
-}
-
-const items: MenuItem[] = [
-	getItem('My Courses', '1', <MailOutlined />),
-	getItem('My Teaching', '2', <CalendarOutlined />),
-	getItem('Messages', '3', <AppstoreOutlined />),
-	getItem('Settings', '4', <SettingOutlined />),
-	getItem(
-		<a href='https://ant.design' target='_blank' rel='noopener noreferrer'>
-			Ant Design
-		</a>,
-		'link',
-		<LinkOutlined />
-	),
-];
-
 const SideMenu = (props: Props) => {
+
+	const [selectedKey, setSelectedKey] = useState<string>("1");
+
+	const getItem = (
+		label: React.ReactNode,
+		key?: React.Key | null,
+		icon?: React.ReactNode,
+		children?: MenuItem[]
+	): MenuItem => {
+		return {
+			key,
+			icon,
+			children,
+			label,
+		} as MenuItem;
+	}
+
+	const renderLinkItem = (label: string, route: string) => {
+		return (
+			<Link to={route}>{label}</Link>
+		);
+	};
+
+	const items: MenuItem[] = [
+		getItem(renderLinkItem("My Courses", Routes.Courses.path), '1', <MailOutlined />),
+		getRole() === Role.PROFESSOR ? getItem('My Teaching', '2', <CalendarOutlined />) : null,
+		getItem(renderLinkItem("Messages", Routes.Messages.path), '3', <AppstoreOutlined />),
+		getItem('Settings', '4', <SettingOutlined />),
+		getItem(
+			<a href='https://ant.design' target='_blank' rel='noopener noreferrer'>
+				Ant Design
+			</a>,
+			'link',
+			<LinkOutlined />
+		),
+	];
+
+	const onSelectItem: MenuProps['onClick'] = (e) => {
+		console.log("clicked", e.key)
+		setSelectedKey(e.key);
+	};
+
 	return (
 		<Menu
 			// style={{ width: 256 }}
-			defaultSelectedKeys={['1']}
-			defaultOpenKeys={['sub1']}
+			// defaultSelectedKeys={["1"]}
+			selectedKeys={[selectedKey]}
+			onClick={onSelectItem}
+			// defaultOpenKeys={['sub1']}
 			mode='vertical'
 			theme='dark'
 			items={items}
--- src/components/utils/session.ts
+++ src/components/utils/session.ts
@@ -22,6 +22,7 @@ const ROLE = "OMNI_ROLE";
 export const getRole = () => {
     const role = localStorage.getItem(ROLE);
     if(!role) {
+        return Role.NONE;
         throw new Error('Role not found');
     }
     return role;
